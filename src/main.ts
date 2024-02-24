8e5d76092fc0be6f2629d0bffaf5aa39db1a48fd
dfealves <daniloferreira.alves@outlook.com>
2024-02-24 16:16:25 -0300

    refactor(general): fix some confi files

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
 import * as fs from 'fs';
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 
 async function bootstrap() {
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
-  const keyPath = './key.pem';
-  const certPath = './cert.pem';
-  
-  const key = fs.readFileSync(keyPath);
-  const cert = fs.readFileSync(certPath)
 
-  const httpsOptions = {
-    key: key,
-    cert: cert,
-  };
-  
   const app = await NestFactory.create(AppModule);
   app.use(cors({
     origin: ['*' ,'https://e198-2804-14d-7e3d-8dce-d49c-4ac2-27c4-4e30.ngrok-free.app'],
@@ -24,7 +12,7 @@ async function bootstrap() {
     credentials: true
   }));  
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT ||3000);
 
 
 }
